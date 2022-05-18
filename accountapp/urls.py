6350a5617b24db7969c26e1fb42c338d2b414d96
KooHyunJung <ghj3160@gmail.com>
2022-05-18 17:58:19 +0900

    use DRF mixins

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accountapp.views import hello_world_drf, BooksAPI, BookAPI
+from accountapp.views import hello_world_drf, BooksAPI, BookAPI, BooksAPIMixins, BookAPIMixins
 
 urlpatterns = [
     path('hello_world_drf/', hello_world_drf),
     path('cbv/books/', BooksAPI.as_view()),
     path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path('mixin/books/', BooksAPIMixins.as_view()),
+    path('mixin/book/<int:bid>/', BookAPIMixins.as_view()),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from accountapp.models import Book
 from accountapp.serializers import BookSerializer
 from rest_framework.generics import get_object_or_404
+# DRF mixins
+from rest_framework import generics
+from rest_framework import mixins
 
 
 # DRF FBV 방식
@@ -13,6 +16,7 @@ def hello_world_drf(request):
     return Response({'msg': 'hello_world!!!'})
 
 
+# DRF CBV 방식
 class BooksAPI(APIView):
     def get(self, request):
         books = Book.objects.all()
@@ -32,3 +36,32 @@ class BookAPI(APIView):
         book = get_object_or_404(Book, bid=bid)
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# DRF mixins
+class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class BookAPIMixins(mixins.RetrieveModelMixin , generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'bid'
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    # 책 1권 수정 메소드
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    # 책 1권 delete 메소드
+    def delete(self, request, *args, **kwargs):
+        return self.destory(request, *args, **kwargs)
